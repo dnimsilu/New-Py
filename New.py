b021aba7a10d1af6c3b160543c0bb8054a605017
dnimsilu <163779849+dnimsilu@users.noreply.github.com>
2024-04-01 17:24:04 +0530

    Create New.py

---
+++ New.py
@@ -0,0 +1,15 @@
+import random
+
+# import my_module
+
+
+random_integer = random.randint(0, 5)
+# print(random_integer)
+
+# print(my_module.myfuntion())
+
+random_float = random.random()
+print(random_float*random_integer)
+
+love_score = random.randint(1,100)
+print(f'your love score is = {love_score} %')
